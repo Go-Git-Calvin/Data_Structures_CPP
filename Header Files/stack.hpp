1e4bcff93668d3650da8ec5393710fd6e8b229a7
Calvin <iam_calvin@icloud.com>
2016-07-04 01:20:32 -0400

    Test

---
+++ Header Files/stack.hpp
@@ -0,0 +1,24 @@
+
+#ifndef STACKS_CPP_STACK_H
+#define STACKS_CPP_STACK_H
+
+class Stack {
+    int max_Stack;
+    int empty_Stack;
+    int top;
+
+    char* items;
+
+public:
+    Stack(int);
+    ~Stack();
+
+    void push(char);
+    char pop();
+
+    int empty();
+    int full();
+
+};
+
+#endif //STACKS_CPP_STACK_H
